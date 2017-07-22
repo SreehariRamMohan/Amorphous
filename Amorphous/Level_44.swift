5b2c6629c5f79e2f0543eec153c310c634e88d0b
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-07-21 19:54:16 -0700

    Finished reorganizing and adding to chapter 1
    Chapter 1 tutorial complete

--- Amorphous/Level_44.swift
+++ Amorphous/Level_44.swift
@@ -1,23 +1,31 @@
 //
-//  Level_1.swift
+//  Level_4.swift
 //  Amorphous
 //
-//  Created by Sreehari Ram Mohan on 7/10/17.
+//  Created by Sreehari Ram Mohan on 7/15/17.
 //  Copyright © 2017 Sreehari Ram Mohan. All rights reserved.
 //
 
 import Foundation
 import SpriteKit
 
-class Level_9: Level {
+class Level_44: Level {
     
     //player variable
     var player: Player!
     var window: SKSpriteNode!
+    var first = true
+    
+    
     override func didMove(to view: SKView) {
         //call did move in parent
         super.didMove(to: view)
         initialize_variables()
+        print("did move of level 4")
+        
+        //level 3 has more obstacles, so we need to zoom the camera out in order for the player to be able to see them
+        let zoomInAction = SKAction.scale(to: 2, duration: 0)
+        cameraNode.run(zoomInAction)
     }
     
     
@@ -28,10 +36,14 @@ class Level_9: Level {
         //tell Level who the player is
         self.setPlayer(player: player)
         
-        print("reset the player in level 1")
+        print("reset the player")
+        print("in level 4")
         
         //add player to the world
         addChild(player)
+        
+        player.position.x = -UIScreen.main.bounds.width/3
+        
     }
     
     override func updateCamera() {
@@ -40,24 +52,28 @@ class Level_9: Level {
                 //the player is far below the screen, display the restart button
                 showRestartButton()
             }
-            let y = clamp(value: currentPlayer.position.y, lower: 20, upper: UIScreen.main.bounds.width/2-10)
-            let x = clamp(value: currentPlayer.position.x, lower: 0 , upper: 5*(UIScreen.main.bounds.width/2) + 165)
+            
+            //will figure out the clamp values later
+            
+//            let y = clamp(value: currentPlayer.position.y, lower: UIScreen.main.bounds.width/4, upper: UIScreen.main.bounds.width/2 - 180)
+//            let x = clamp(value: currentPlayer.position.x, lower: (UIScreen.main.bounds.height)/2 + 100 , upper: (7*(UIScreen.main.bounds.height/2) + 190))
             //clamp with level 1 dimensions in mind
-            cameraNode.position.x = x
-            cameraNode.position.y = y
+            
+            
+            cameraNode.position.x = currentPlayer.position.x
+            cameraNode.position.y = currentPlayer.position.y
         }
     }
     
     override func hintButtonPressed() {
         //Pan the camera around the level to alert the player of what obstacles are to come
         //Pan the camera from where the player is currently located to the location of the window(for them to escape)
-        
         window = self.childNode(withName: "//window") as! SKSpriteNode
         let windowPos: CGPoint = self.convert(CGPoint(x:0, y:0), from: window)
         let myPosition = self.position
         
-        let moveAction = SKAction.move(to: CGPoint(x: windowPos.x, y: myPosition.y), duration: 5)
-        let moveAction2 = SKAction.move(to: CGPoint(x: myPosition.x, y: myPosition.y), duration: 5)
+        let moveAction = SKAction.move(to: CGPoint(x: windowPos.x, y: windowPos.y), duration: 3)
+        let moveAction2 = SKAction.move(to: CGPoint(x: myPosition.x, y: myPosition.y), duration: 3)
         let sequence = SKAction.sequence([moveAction, moveAction2])
         
         cameraNode.run(sequence)
--- Amorphous/Level_5.swift
+++ Amorphous/Level_5.swift
@@ -2,29 +2,22 @@
 //  Level_5.swift
 //  Amorphous
 //
-//  Created by Sreehari Ram Mohan on 7/17/17.
+//  Created by Sreehari Ram Mohan on 7/21/17.
 //  Copyright © 2017 Sreehari Ram Mohan. All rights reserved.
 //
 
+
 import Foundation
 import SpriteKit
-
 class Level_5: Level {
     
     //player variable
     var player: Player!
     var window: SKSpriteNode!
-    var first = true
-    
     override func didMove(to view: SKView) {
         //call did move in parent
         super.didMove(to: view)
         initialize_variables()
-        print("did move of level 5")
-        
-        //level 5 has more obstacles, so we need to zoom the camera out in order for the player to be able to see them
-        let zoomInAction = SKAction.scale(to: 2, duration: 1)
-        cameraNode.run(zoomInAction)
     }
     
     
@@ -35,13 +28,11 @@ class Level_5: Level {
         //tell Level who the player is
         self.setPlayer(player: player)
         
-        print("reset the player")
-        print("in level 5")
+        print("reset the player in level 5")
         
         //add player to the world
         addChild(player)
         
-        player.position.x = -UIScreen.main.bounds.width/3
     }
     
     override func updateCamera() {
@@ -50,8 +41,8 @@ class Level_5: Level {
                 //the player is far below the screen, display the restart button
                 showRestartButton()
             }
-            let y = clamp(value: currentPlayer.position.y, lower: UIScreen.main.bounds.width/4, upper: UIScreen.main.bounds.width/2 - 180)
-            let x = clamp(value: currentPlayer.position.x, lower: (UIScreen.main.bounds.height)/2 + 100 , upper: (13*(UIScreen.main.bounds.height/2) + 190))
+            let y = clamp(value: currentPlayer.position.y, lower: 20, upper: 2*UIScreen.main.bounds.width/2-10)
+            let x = clamp(value: currentPlayer.position.x, lower: 20 , upper: 1*(UIScreen.main.bounds.width/3))
             //clamp with level 1 dimensions in mind
             cameraNode.position.x = x
             cameraNode.position.y = y
@@ -61,12 +52,13 @@ class Level_5: Level {
     override func hintButtonPressed() {
         //Pan the camera around the level to alert the player of what obstacles are to come
         //Pan the camera from where the player is currently located to the location of the window(for them to escape)
+        
         window = self.childNode(withName: "//window") as! SKSpriteNode
         let windowPos: CGPoint = self.convert(CGPoint(x:0, y:0), from: window)
         let myPosition = self.position
         
-        let moveAction = SKAction.move(to: CGPoint(x: windowPos.x - 430, y: UIScreen.main.bounds.width/4), duration: 5)
-        let moveAction2 = SKAction.move(to: CGPoint(x: myPosition.x + 300, y:  UIScreen.main.bounds.width/4), duration: 5)
+        let moveAction = SKAction.move(to: CGPoint(x: windowPos.x, y: windowPos.y), duration: 3)
+        let moveAction2 = SKAction.move(to: CGPoint(x: myPosition.x, y: myPosition.y), duration: 3)
         let sequence = SKAction.sequence([moveAction, moveAction2])
         
         cameraNode.run(sequence)
